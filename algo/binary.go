f9203bfdd58dc16372a826b36e501e1ec9e95428
tsong <tsong@zshield.net>
2020-05-14 10:42:06 +0800

    二分查找

--- algo/binary.go
+++ algo/binary.go
@@ -10,8 +10,68 @@ func BinarySearch(data []int, value int) int {
 		if v == value {
 			return mid
 		}
-		if value > v {
+		if v > value {
+			e = mid - 1
+		} else {
 			s = mid + 1
+		}
+	}
+	return -1
+}
+
+// BinaryEqualFirst 查找第一个值等于给定值的元素
+func BinaryEqualFirst(data []int, value int) int {
+	s := 0
+	e := len(data) - 1
+	for s <= e {
+		mid := (e-s)/2 + s
+		v := data[mid]
+
+		if v > value {
+			e = mid - 1
+		} else if v < value {
+			s = mid + 1
+		} else if mid == 0 || data[mid-1] != value {
+			return mid
+		} else {
+			e = mid - 1
+		}
+
+	}
+	return -1
+}
+
+// BinaryEqualEnd 查找最后一个值等于给定值的元素
+func BinaryEqualEnd(data []int, value int) int {
+	s := 0
+	e := len(data) - 1
+	for s <= e {
+		mid := s + (e-s)/2
+		v := data[mid]
+		if v > value {
+			e = mid - 1
+		} else if v < mid {
+			s = mid + 1
+		} else if mid == len(data)-1 || data[mid+1] != value {
+			return mid
+		} else {
+			s = mid + 1
+		}
+	}
+	return -1
+}
+
+// BinaryGteFirst 查找第一个大于等于给定值的元素
+func BinaryGteFirst(data []int, value int) int {
+	s := 0
+	e := len(data) - 1
+	for s <= e {
+		mid := s + (e-s)/2
+		v := data[mid]
+		if v < value {
+			s = mid + 1
+		} else if mid == 0 || data[mid-1] < value {
+			return mid
 		} else {
 			e = mid - 1
 		}
@@ -19,22 +79,20 @@ func BinarySearch(data []int, value int) int {
 	return -1
 }
 
-// BinaryEqualFirst 查找第一个值等于给定值的元素
-func BinaryEqualFirst() {
-
-}
-
-// BinaryEqualEnd 查找最后一个值等于给定值的元素
-func BinaryEqualEnd() {
-
-}
-
-// BinaryGteFirst 查找第一个大于等于给定值的元素
-func BinaryGteFirst() {
-
-}
-
 // BinaryLteEnd 查找最后一个小于等于给定值的元素
-func BinaryLteEnd() {
-
+func BinaryLteEnd(data []int, value int) int {
+	s := 0
+	e := len(data) - 1
+	for s <= e {
+		mid := s + (e-s)/2
+		v := data[mid]
+		if v > value {
+			e = mid - 1
+		} else if mid == len(data)-1 || data[mid+1] > value {
+			return mid
+		} else {
+			s = mid + 1
+		}
+	}
+	return -1
 }
--- cmd/binary.go
+++ cmd/binary.go
@@ -15,10 +15,25 @@ var binaryCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("binary called")
 		fmt.Println("equal search")
-		data := []int{1, 5, 6, 13, 21, 58, 69}
-		v := algo.BinarySearch(data, 13)
-		fmt.Printf("search %d at index: %d\n", 13, v)
-		fmt.Printf("search %d at index: %d\n", 14, algo.BinarySearch(data, 14))
+		data := []int{1, 5, 6, 13, 13, 13, 13, 13, 14, 21, 58, 69}
+		v := algo.BinarySearch(data, 14)
+		fmt.Printf("search %d at index: %d\n", 14, v)
+
+		fmt.Println("equal first")
+		v = algo.BinaryEqualFirst(data, 13)
+		fmt.Printf("search first equal %d at index: %d\n", 13, v)
+
+		fmt.Println("equal last")
+		v = algo.BinaryEqualEnd(data, 13)
+		fmt.Printf("search first equal %d at index: %d\n", 13, v)
+
+		fmt.Println("gte first")
+		v = algo.BinaryGteFirst(data, 15)
+		fmt.Printf("search gte %d at index: %d\n", 15, v)
+
+		fmt.Println("lte en")
+		v = algo.BinaryLteEnd(data, 15)
+		fmt.Printf("search lte %d at index: %d\n", 15, v)
 	},
 }
 
