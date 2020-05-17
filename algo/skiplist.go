c54d4908e34323e2706ce73d16be735d4a23ad5f
WardSong <1556824234@qq.com>
2020-05-17 11:59:43 +0800

    跳表的实现

--- algo/skiplist.go
+++ algo/skiplist.go
@@ -1,125 +1,112 @@
+/*
+Package algo skiplist
+skiplist 跳表实现:
+1. SkipNode 跳表中的节点:
+	- next []*SkipNode 表示当前节点的层高，以及每一层指向的后续节点
+2. SkipList
+	- CreateNode 创建节点，决定新插入节点的层高
+	- find: 值查找，返回每层遍历的最有一个节点，这些节点是每层插入的前驱节点
+		- 从顶层往底层查找
+		- 每一层从前往后查找
+		- 每一层终止的条件是，下一个节点的 Key >= num(待查找值)，并将当前节点保存下来*/
 package algo
 
 import (
-	"fmt"
 	"math"
 	"math/rand"
 )
 
-const (
-	maxLevel           = 25
-	defaultProbability = 1 / math.E
-)
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-// SkipElement 存入跳表中的值必须满足的接口
-type SkipElement interface {
-	Key() string
-	String() string
-}
-
 // SkipNode 调表表节点
 type SkipNode struct {
-	level int
-	key   float64
-	value SkipElement
-	next  []*SkipNode
+	key  int
+	next []*SkipNode
 }
 
-// SkipList 跳表
-type SkipList struct {
-	max    int // 调表的最高层高
-	level  int // 调表的实际层高
-	head   *SkipNode
-	lenght int
+// Skiplist 跳表
+type Skiplist struct {
+	level int // 跳表的层高
+	head  *SkipNode
+	size  int
 
+	// 层高控制
 	levelUpBaseNum int32 // 每多少个节点增加一层
 }
 
-// NewSkipList 创建 SkipList
-func NewSkipList(level int, basenum int32) *SkipList {
-	return &SkipList{
-		max:            level,
-		level:          1,
-		lenght:         0,
-		levelUpBaseNum: basenum,
-		head: &SkipNode{level: level, key: math.MinInt32, value: nil,
+// Constructor 创建 SkipList
+func Constructor() Skiplist {
+	level := 10
+	return Skiplist{
+		level:          level,
+		levelUpBaseNum: 3,
+		head: &SkipNode{key: math.MinInt64,
 			next: make([]*SkipNode, level, level)},
 	}
 }
 
-func (s *SkipList) String() {
-	for i := s.max - 1; i >= 0; i-- {
-		fmt.Printf("head%d", i)
-		c := "-"
-		l0 := s.head
-		cur := s.head
-		for l0.next[0] != nil {
-			// fmt.Println(l0.key)
-			l0 = l0.next[0]
-			if l0 != cur.next[i] {
-				fmt.Printf("--")
-			} else {
-				cur = cur.next[i]
-				fmt.Printf("%s%d", c, int(cur.key))
-			}
-		}
-		fmt.Println()
-	}
-}
-
 // CreateNode 创建 SkipNode
-func (s *SkipList) CreateNode(key float64, value SkipElement) *SkipNode {
+func (s *Skiplist) CreateNode(key int) *SkipNode {
 	level := 1
-	for i := 1; i <= s.max; i++ {
+	for i := 1; i < s.level; i++ { // i < s.level
 		r := rand.Int31()
-		fmt.Printf("random:  %d, %d\n", r, r%s.levelUpBaseNum)
 		if r%s.levelUpBaseNum == 1 {
 			level++
 		}
 	}
-	fmt.Printf("New Node level: %d, Skip Level: %d\n", level, s.level)
-	return &SkipNode{key: key, value: value, level: level,
+	return &SkipNode{key: key,
 		next: make([]*SkipNode, level, level)}
 }
 
 // Find 节点查找
-func (s *SkipList) find(key float64) (pre []*SkipNode, ok bool) {
-	return nil, false
-}
-
-// Find 查找
-func (s *SkipList) Find(key float64) (SkipElement, bool) {
-	// 不能使用 find() 方法，因为无法知道是在第几层查找到的值
+func (s *Skiplist) find(num int) ([]*SkipNode, bool) {
 	cur := s.head
+	pre := make([]*SkipNode, s.level, s.level)
 	for i := s.level - 1; i >= 0; i-- {
 		for cur.next[i] != nil {
-			k := cur.next[i].key
-			v := cur.next[i].value
-			if k == key {
-				return v, true
-			}
-			if k > key {
+			key := cur.next[i].key
+			if key >= num {
+				pre[i] = cur
 				break
 			}
 			cur = cur.next[i]
 		}
+		if cur.next[i] == nil {
+			pre[i] = cur
+		}
 	}
-	return nil, false
+	last := pre[0].next[0]
+	ok := false
+	if last != nil && last.key == num {
+		ok = true
+	}
+	return pre, ok
 }
 
-// Insert 插入
-func (s *SkipList) Insert(key float64, value SkipElement) {
-
+// Search 查找值是否存在
+func (s *Skiplist) Search(target int) bool {
+	_, ok := s.find(target)
+	return ok
 }
 
-// Delete 删除节点
-func (s *SkipList) Delete(key float64) (SkipElement, bool) {
-	return nil, false
+// Add 添加
+func (s *Skiplist) Add(num int) {
+	pre, _ := s.find(num)
+	node := s.CreateNode(num)
+	for i := len(node.next) - 1; i >= 0; i-- {
+		node.next[i] = pre[i].next[i]
+		pre[i].next[i] = node
+	}
+}
+
+// Erase 删除
+func (s *Skiplist) Erase(num int) bool {
+	pre, ok := s.find(num)
+	if !ok {
+		return ok
+	}
+	node := pre[0].next[0]
+	for i := len(node.next) - 1; i >= 0; i-- {
+		pre[i].next[i] = pre[i].next[i].next[i]
+	}
+	node.next = nil
+	return ok
 }
