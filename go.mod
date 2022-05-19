07aac28c7e8ddd2988ebe509da6d280a20028c49
tsong <tsong@163.com>
2022-05-19 20:44:16 +0800

    gin 示例代码

--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module github.com/hotttao/goalgo
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cobra v1.4.0
 	github.com/spf13/viper v1.10.1
 	github.com/stretchr/testify v1.7.1 // indirect
+	github.com/ugorji/go v1.2.7 // indirect
+	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167 // indirect
 	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- web/http_server.go
+++ web/http_server.go
@@ -1,14 +1,55 @@
 package web
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
+func AuthHandler(word string) func(c *gin.Context) {
+	gin.Recovery()
+	return func(c *gin.Context) {
+		c.Next()
+		c.JSON(200, gin.H{"visit": word})
+	}
+}
+
 func StartWebServer() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, %q", "tsong")
+	// Creates a router without any middleware by default
+	r := gin.New()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
+
+	r.GET("/test", func(c *gin.Context) {
+		c.Request.URL.Path = "/test2"
+		r.HandleContext(c)
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r.GET("/test2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"hello": "world"})
+	})
+
+	// Authorization group
+	// authorized := r.Group("/", AuthRequired())
+	// exactly the same as:
+	authorized := r.Group("/")
+	// per group middleware! in this case we use the custom created
+	// AuthRequired() middleware just in the "authorized" group.
+	// authorized.Use(gin.AuthRequired())
+	{
+		authorized.GET("/login", AuthHandler("logini"))
+		authorized.GET("/submit", AuthHandler("submit"))
+		authorized.GET("/read", AuthHandler("read"))
+
+		// nested group
+		testing := authorized.Group("testing")
+		// visit 0.0.0.0:8080/testing/analytics
+		testing.GET("/analytics", AuthHandler("testing/analytics"))
+	}
+
+	// Listen and serve on 0.0.0.0:8080
+	r.Run(":8080")
 }
